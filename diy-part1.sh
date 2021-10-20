cd1c8fa165d5e71007d2b0eb62b07db0cdfbcb28
jiangxi14520 <jiangxi14520@gmail.com>
2021-10-20 12:17:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git install-program https://github.com/tuanqing/install-program.git' >>feeds.conf.default
