dc24837fbd12ac32ff1474b1c7209f049208591e
jiangxi14520 <jiangxi14520@gmail.com>
2021-10-22 08:37:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 git clone https://github.com/tuanqing/install-program package/install-program
+export GO111MODULE=on
+export GOPROXY=https://goproxy.io
